6476edd08e6b1139e6d32ca483897e6f1e69df6b
문성훈 <munseonghun@munseonghun-ui-Macmini.local>
2022-08-11 19:05:34 +0900

    sql fix2

--- data/tweets.js
+++ data/tweets.js
@@ -1,11 +1,11 @@
 import { db } from '../db/database.js';
 
   
-  const SELECT_JOIN = 'SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id';
-  const ORDER_DESC = 'ORDER BY tw.createdAt DESC'
+  const SELECT_JOIN = "SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id";
+  const ORDER_DESC = "ORDER BY tw.createdAt DESC";
   export async function getAll() {
     return db
-    .execute(`${SELECT_JOIN}${ORDER_DESC}`)
+    .execute(`${SELECT_JOIN} ${ORDER_DESC}`)
   .then((result) => result[0])
   }
   
