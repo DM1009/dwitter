04fd0dec41da583aafdb016dbbbd4fe24e8db31e
문성훈 <munseonghun@munseonghun-ui-Macmini.local>
2022-08-12 22:45:02 +0900

    app.js

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.use((req, res, next) => {
     res.sendStatus(500);
   });
 
-  app.listen(config.port)  
+  app.listen(config.port || 8080)
 
 
--- config.js
+++ config.js
@@ -23,7 +23,6 @@ export const config = {
     user: required('DB_USER'),
     database: required('DB_DATABASE'),
     password: required('DB_PASSWORD'),
-    port: required('DB_PORT')
   },
   cors: {
     allowedOrigin: required('CORS_ALLOW_ORIGIN')
